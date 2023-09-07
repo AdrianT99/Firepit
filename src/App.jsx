0ddfe0d61fee5809743920272e36e945b35dd486
AdrianT99 <adriantenorior99@gmail.com>
2023-09-06 22:35:35 -0600

    Addition of new layout components for the MainNavigation header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+//import './App.css'
+
+import MainNavigation from "./components/Layout/MainNavigation"
+
+
 
 function App() {
 
+
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <MainNavigation />
   )
 }
 
